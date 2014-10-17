50e9fbbcf3429f75c66ec2663405920330a5dcfd
Dale Emery <dale@dhemery.com>
2014-10-17 14:25:21 -0700

    Use standard config file and host.yaml

--- config.rb
+++ config.rb
@@ -1,27 +1,19 @@
 site = OpenStruct.new(YAML::load_file(File.join('data', 'site.yaml')))
+host = OpenStruct.new(YAML::load_file(File.join('data', 'host.yaml')))
 
 Time.zone = site['timezone']
 
 set :layout, :article
+
 set :haml, {ugly: true}
 
 activate :deploy do |deploy|
+  deploy.build_before = true
   deploy.method = :rsync
-  deploy.host   = 'dhemery.com'
-  deploy.user   = 'dhemery'
-  deploy.path   = '/srv/sites/dhemery.com'
+  deploy.host   = host.domain
+  deploy.user   = host.user 
+  deploy.path   = File.join(host.root, site.domain)
   deploy.flags  = '-avz -e ssh --delete'
 end
 
 activate :directory_indexes
-
-helpers do
-  def relativize url
-    uri = URI.parse(url)
-    if uri.host == data.site.domain
-      uri.route_from "http://#{data.site.domain}"
-    else
-      uri
-    end
-  end
-end
